1fd553d4981c5e4aeb489e7e0f01e128352232e7
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-26 18:26:58 -0500

    Just fixed indents on base and added comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,43 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
+
 '''
 Module for AirBnB console
 '''
 
+
 class BaseModel:
     '''
-    
+    Base class for all AirBnB models.
     '''
+
     def __init__(self):
+        '''
+        Initializes a new BaseModel instance.
+        '''
         self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = self.created_at
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-	def __str__(self):
-    	'''
-     
-    	'''
-	    return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
-	
-	def save(self):
-     	'''
-     
-    	 '''
-		self.updated_at = datetime.now()
+    def __str__(self):
+        '''
+        Returns a string representation of the BaseModel instance.
+        '''
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-	def to_dict(self):
-     	'''
-     
-     	'''
-		dict_repr = self.__dict__.copy()
-		dict_repr['__class__'] = self.__class__.__name__
-		dict_repr['created_at'] = self.created_at.isoformat()
-		dict_repr['updated_at'] = self.updated_at.isoformat()
-		return dict_repr
+    def save(self):
+        '''
+        Updates the 'updated_at' attribute with the current datetime.
+        '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''
+        Returns a dictionary representation of the BaseModel instance.
+        '''
+        dict_repr = self.__dict__.copy()
+        dict_repr['__class__'] = self.__class__.__name__
+        dict_repr['created_at'] = self.created_at.isoformat()
+        dict_repr['updated_at'] = self.updated_at.isoformat()
+        return dict_repr
