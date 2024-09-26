93f7c3154cea535bc19e00dd23e126aadf119a78
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-26 18:51:27 -0500

    Update to base to ideally make checker happy and all the peasants to rejoice.

--- models/base_model.py
+++ models/base_model.py
@@ -2,46 +2,53 @@
 import uuid
 from datetime import datetime
 
-'''
-Module for AirBnB console
-
-'''
-
 
 class BaseModel:
-    '''
+    """
     Base class for all AirBnB models.
-    
+
     Attributes:
         id (str): Unique identifier for the object.
         created_at (datetime): Date and time the object was created.
         updated_at (datetime): Date and time the object was last updated.
-    '''
+    """
 
     def __init__(self, *args, **kwargs):
-        '''
+        """
         Initializes a new BaseModel instance.
-        '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+
+        Args:
+            *args: argument list
+            **kwargs: Keyword arguments representing attributes.
+        """
+        if kwargs:
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        '''
-        Returns a string representation of the BaseModel instance.
-        '''
+        """
+        Returns a string representation of the Base instance.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        '''
-        Updates the 'updated_at' attribute with the current datetime.
-        '''
+        """
+        Updates the 'updated_at' attribute with the current date/time.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        '''
-        Returns a dictionary representation of the BaseModel instance.
-        '''
+        """
+        Returns a dictionary representation of the Base instance.
+        """
         dict_repr = self.__dict__.copy()
         dict_repr['__class__'] = self.__class__.__name__
         dict_repr['created_at'] = self.created_at.isoformat()
