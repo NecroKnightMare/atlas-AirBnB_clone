a0a629cd3e6e969c6432e51e381be704da9dee02
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-27 18:59:55 -0500

    made pep8 oriented to get points

--- console.py
+++ console.py
@@ -13,31 +13,30 @@ entry point to the Command interpreter or prompt
 class HBNBCommand(cmd.Cmd):
     '''
     entry point of the command interpreter
-    
         Methods:
             do_quit: quit command to end program
             do_EOF: EOF command to exit program
-            emptyline: empty line     
+            emptyline: empty line
     '''
     prompt = '(hbnb)'
-    
+
     def do_quit(self, arg):
         return True
-    
+
     def do_EOF(self, arg):
         return True
 
     def emptyline(self, arg):
         pass
 
+
 class BaseModel:
     '''
     create new instance of BaseModel
     '''
-    
+
     def __init__(self, *args, **kwargs):
         '''
-        
         '''
         if kwargs:
             for key, value in kwargs.items():
@@ -47,36 +46,34 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def save(self):
         '''
-        
         '''
         self.updated_at = datetime.now()
         storage.save(self)
-    
+
     def to_dict(self):
         '''
-        
         '''
         dict_repr = self.__dict__.copy()
         dict_repr['__class__'] = self.__class__.__name__
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
         return dict_repr
-    
+
     def __str__(self):
-    
         """
         Returns a string representation of the Base instance.
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
+
     class FileStorage:
         '''
         use nathans functions here and update
         '''
-        
+
 if __name__ == '__main__':
     storage.reload()  # Call reload here
     console().cmdloop()
