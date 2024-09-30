89778586699b17aa996d34331ba6df38d1e4a796
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-09-30 23:55:31 +0000

    added base Model test; made adjustments to check one test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,32 +1,21 @@
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
     '''
     unittest for BaseModel
-    '''
+    
     def test_instance(self):
-        '''
+        
         test class inheritance
-        '''
+        
         model = BaseModel()
         self.assertIsInstance(model, BaseModel)
         self.assertIsInstance(model, id, str)
         self.assertIsInstance(model.created_at, datetime)
         self.assertIsInstance(model.updated_at, datetime)
-        
-    def test_save(self):
-        '''
-        test for save method
-        '''
-        model = BaseModel()
-        old_update_at = model_updated_at
-        model.save()
-        self.assertEqual(model_updated_at, old_updated_at)
-
-        
+        '''        
     def test_to_dict(self):
         '''
         test to see if its equal outputs
