17c6cba7418fa0d82bb788c18cebe76de2e3c437
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-01 22:40:00 +0000

    moving tests

---
+++ tests/test_city.py
@@ -0,0 +1,48 @@
+import unittest
+from models.city import City
+
+class TestCity(unittest.Testcase):
+    '''
+    tests for City class
+    '''
+    def test_initialization(self):
+        '''
+        test to check if City is from City and if name is empty
+        '''
+        city = City()
+        self.assertIsInstance(city, City)
+        self.assertEqual(city.name, "")
+        self.assertEqual(city.state_id, "")
+
+    def test_atters(self):
+        '''
+        test attributtes for city
+        '''
+        city = City()
+        city.name = "Texas"
+        city.state_id = ""
+        self.assertEqual(city.name, "Dallas")
+
+    def test_to_dict_method(self):
+        '''
+        tests to check if part of dictionary
+        '''
+        city = City()
+        city_dict = city.to_dict()
+        self.assertIsInstance(city_dict, dict)
+        self.assertEqual(city_dict['__class__'], 'City')
+        self.assertEqual(city_dict['name'], "")
+        self.assertEqual(city_dict['state_id'], "")
+
+    def test_save_method(self):
+        '''
+        test to see if save works properly
+        '''
+        city = City()
+        old_update_at = city.updated_at 
+        city.save()
+        self.assertNotEqual(city.updated_at, old_updated_at)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ tests/test_state.py
@@ -0,0 +1,45 @@
+import unittest
+from models.state import State
+
+class TestState(unittest.Testcase):
+    '''
+    tests for State class
+    '''
+    def test_initialization(self):
+        '''
+        test to check if State is from State and if name is empty
+        '''
+        state = State()
+        self.assertIsInstance(state, State)
+        self.assertEqual(state.name, "")
+
+    def test_atters(self):
+        '''
+        test attributtes for state
+        '''
+        state = State()
+        state.name = "Texas"
+        self.assertEqual(state.name, "Texas")
+
+    def test_to_dict_method(self):
+        '''
+        tests to check if part of dictionary
+        '''
+        state = State()
+        state_dict = state.to_dict()
+        self.assertIsInstance(state_dict, dict)
+        self.assertEqual(state_dict['__class__'], 'State')
+        self.assertEqual(state_dict['name'], "")
+
+    def test_save_method(self):
+        '''
+        test to see if save works properly
+        '''
+        state = State()
+        old_updated_at = state.updated_at 
+        state.save()
+        self.assertNotEqual(state.updated_at, old_updated_at)
+
+if __name__ == '__main__':
+    unittest.main()
+
