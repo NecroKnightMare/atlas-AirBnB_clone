68b1b15454638dc824a3083c9c8289b5b79ae283
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-10-01 17:42:06 -0500

    Simplify

--- models/state.py
+++ models/state.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
 from models.base_model import BaseModel
 
-'''
-State class that inherits from BaseModel
-'''
-
-
 class State(BaseModel):
-    '''
-    State class that inherits from BaseModel
-    
-    Attr:
-        name: state string
-    '''
+    """
+    Represents a state.
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes a new State instance.
-        """
-        super().__init__(*args, **kwargs)
-        self.name = kwargs.get('name', "")  # Access name from kwargs
-        name = ""
+    Attributes:
+        name (str): The name of the state.
+    """
+    name = "Default State Name"
