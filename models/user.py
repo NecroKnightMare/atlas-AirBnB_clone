ade19e3e0205e045fd2aadac69d27cc10e8db2c4
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-09-30 20:43:08 -0500

    Modified user for test

--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 from models.base_model import BaseModel
 
-class User(BaseModel):
-    def __init__(self):
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-        
+
+class User(BaseModel):  # Inherit from BaseModel
+    """
+    Represents a user in the AirBnB system.
+
+    Attributes:
+        email (str): The user's email address.
+        password (str): The user's password (hashed in a real application).
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    # No need for __init__ if it's just setting default attr values, currently at least.
