b168800f4ec0cb866ab23b2a73cd19d6816e3ded
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-01 20:07:34 +0000

    created test for user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,14 @@
+import unittest
+from models.base_model import BaseModel
+from models import storage
+from datetime import datetime
+
+class TestUser(unittest.Testcase):
+    '''
+    unittest for User
+    '''
+    def test_user(self):
+        '''
+
+        '''
+        user = User()
