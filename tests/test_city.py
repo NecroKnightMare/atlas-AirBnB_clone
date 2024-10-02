ea5d91001c5457036cdf41f1b35125488d71eb05
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-01 23:27:47 -0500

    got rid of to_dict method

--- tests/test_city.py
+++ tests/test_city.py
@@ -17,19 +17,18 @@ class TestCity(unittest.TestCase):
         '''
         test attributtes for city
         '''
-        city = City(name="Tulsa", state_id="OK")
-        self.assertEqual(city.name, "Tulsa")
-        self.assertEqual(city.state_id, "OK")
-
+        city = City()
+        self.assertEqual(city.name, "")
+        self.assertEqual(city.state_id, "")
+    '''
     def test_to_dict_method(self):
         '''
-        tests to check if part of dictionary
         '''
         city = City(name="Tulsa", state_id="OK")
         city_dict = city.to_dict()
         self.assertEqual(city_dict['name'], "Tulsa")
         self.assertEqual(city_dict['state_id'], "OK")
-
+    '''
         
 
 if __name__ == '__main__':
